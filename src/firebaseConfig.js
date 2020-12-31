665faf5446ec182247ec8deb21eaac2944439866
SINC Mac Mini 1 <roy.delacruz@sinc.business>
2020-12-31 19:05:28 +0300

    Integrated firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+
+
+
+export default firebaseConfig =  {
+    apiKey: "AIzaSyBWipUfR72p8cNG032l0hA_vbcKZO6fUfk",
+    authDomain: "firestore-tutorial-fireship.firebaseapp.com",
+    projectId: "firestore-tutorial-fireship",
+    storageBucket: "firestore-tutorial-fireship.appspot.com",
+    messagingSenderId: "1051127971899",
+    appId: "1:1051127971899:web:6fd908f19cdf1e7ff1dc1e"
+}
+
+
+
